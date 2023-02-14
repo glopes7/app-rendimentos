e6ae29a038b64af038589ec0cefdb332ba77ef9d
Gustavo Lopes <gustavoclash3@gmail.com>
2023-02-13 23:37:06 -0300

    add (qoute boxs, daisyUI)

---
+++ src/components/QuoteBox.tsx
@@ -0,0 +1,35 @@
+export function QuoteBox() {
+  return (
+    <div className="card w-96  bg-base-100 shadow-xl">
+      <div className="card-body">
+        <div className="form-control w-full max-w-xs">
+          <input
+            type="text"
+            placeholder="Ativo"
+            className="input input-bordered w-32"
+          />
+        </div>
+        <div className="flex justify-between items-center">
+          <label>Dividend Yeld</label>
+          <label className="w-auto input-group">
+            <input
+              className=" input input-sm input-bordered w-16"
+              type="number"
+            />
+            <span className="bg-primary">&nbsp;%</span>
+          </label>
+        </div>
+        <div className="flex justify-between items-center">
+          <label>Ultimo Rendimento</label>
+          <label className="w-auto input-group">
+            <input
+              className=" input input-sm input-bordered w-16"
+              type="number"
+            />
+            <span className="bg-primary">R$</span>
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import "/src/index.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QuoteBox } from "./components/QuoteBox";
+import Home from "./pages/Home";
+import "/src/index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/test",
+    element: <QuoteBox/>
+  }
+  
+  
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-            
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
