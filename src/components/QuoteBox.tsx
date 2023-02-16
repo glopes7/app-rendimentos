98a1cba2947e4b506da3b30b6c62735de43e1d06
Gustavo Lopes <gustavoclash3@gmail.com>
2023-02-16 12:40:47 -0300

    a

--- src/components/QuoteBox.tsx
+++ src/components/QuoteBox.tsx
@@ -1,36 +1,47 @@
 export function QuoteBox() {
   return (
     <div className="card w-96  bg-base-100 shadow-xl">
-      <div className="card-body">
+      <div className=" card-body">
         <div className="flex justify-between items-center  ">
           <input
             type="text"
             placeholder="Ativo"
             className="input input-bordered w-32"
           />
-          <div className="flex flex-col gap-1 ">
-            <input
-              type="text"
-              className=" input input-bordered w-36 h-4 py-4 "
-              disabled
-            />
-            <input
-              type="text"
-              className=" input input-bordered w-36 h-4 py-4 "
-              disabled
+          <div className="flex flex-col ">
+            <div className="form-control w-full max-w-xs items-center">
+              <label className=" label">
+                <span className="label-text font-inter font-extralight">
+                  Quantidade de Cotas
+                </span>
+              </label>
+              <input
+                placeholder="Quantidade"
+                type="text"
+                className=" input input-sm input-bordered w-36 py-4 "
+                disabled
               />
-
-              
+            </div>
           </div>
         </div>
         <div className="flex justify-between items-center">
-          <label>Dividend Yeld</label>
+          <label>Valor da Cota</label>
           <label className="w-auto input-group">
             <input
               className=" input input-sm input-bordered w-20"
               type="number"
             />
-            <span className="bg-primary">&nbsp;%</span>
+            <span className="bg-primary">R$</span>
+          </label>
+        </div>
+        <div className="flex justify-between items-center">
+          <label>Dividend Yeld</label>
+          <label className="w-auto input-group">
+            <input
+              className=" input input-sm input-bordered w-20 "
+              type="number"
+            />
+            <span className="  bg-primary">&nbsp;%</span>
           </label>
         </div>
         <div className="flex justify-between items-center">
@@ -40,7 +51,7 @@ export function QuoteBox() {
               className=" input input-sm input-bordered w-20"
               type="number"
             />
-            <span className="bg-primary">R$</span>
+            <span className=" bg-primary">R$</span>
           </label>
         </div>
       </div>
